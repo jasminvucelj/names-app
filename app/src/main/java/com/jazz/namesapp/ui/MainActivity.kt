6deed12537755277b054a20c4b4cf09ab4bb5b03
jasminvucelj <jasminvucelj@users.noreply.github.com>
2024-08-29 13:47:44 +0200

    Add localization

--- app/src/main/java/com/jazz/namesapp/ui/MainActivity.kt
+++ app/src/main/java/com/jazz/namesapp/ui/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.jazz.namesapp.ui
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -11,7 +11,7 @@ import com.jazz.namesapp.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
             val isDarkTheme by remember { mutableStateOf(false) }
 
             AppTheme(darkTheme = isDarkTheme) {
-                MyScreen(viewModel)
+                MainScreen(viewModel)
             }
         }
     }
